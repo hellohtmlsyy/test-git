fd9073cec6eae334ac0efb1cc9df5ec51bf5f3b6
zhengjie.sheng <zhengjie.sheng@horizon.ai>
2023-03-24 18:36:34 +0800

    dev分支修改js文件

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
  * @Author: 圣正杰
  * @Date: 2023-03-24 16:58:57
  * @LastEditors: 圣正杰
- * @LastEditTime: 2023-03-24 18:26:20
+ * @LastEditTime: 2023-03-24 18:36:16
  * @Description: 
  * @FilePath: /test.git/index.js
  */
 console.log('index.js')
-console.log('dev分支代码')
+console.log('dev分支的代码')
